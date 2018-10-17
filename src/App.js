798adcb149c0441388881137b1730ebfbf6ec9cc
Ribo <ribomo42@gmail.com>
2018-10-17 19:34:59 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// const axios = require('axios');
+// var apigClientFactory = require('aws-api-gateway-client').default;
+var apigClientFactory = require('aws-api-gateway-client').default;
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {value: '', result: ''}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    var config = {
+      invokeUrl:'https://1kptyukchh.execute-api.us-east-1.amazonaws.com',
+      apiKey: 'BGlBOG8E4u1BvGrMGLYyN2Zc2VOuCafgIUGD3bt3',
+      region: 'us-east-1'
+    }
+    var apigClient = apigClientFactory.newClient(config);
+    var pathParams = {
+      //This is where path request params go. 
+  };
+    var pathTemplate = '/prod/chatbot';
+    var method = 'POST';
+    var body = {
+      //This is where you define the body of the request
+      messages: [
+        {
+          type: "String", 
+          unstructured: {
+            id: "",
+            text: this.state.value,
+            timestamp: ""
+          }
+        }
+      ]
+    };
+    var additionalParams = {
+
+    }
+    apigClient.invokeApi(pathParams, pathTemplate, method, additionalParams, body)
+    .then((result)=>{
+        //This is where you would put a success callback
+        this.setState({result: result.data})
+        console.log(result)
+    }).catch( function(result){
+        //This is where you would put an error callback
+        console.log(result)
+    });
+
+
+    // axios.get('http://example.com/')
+    //   .then(function (response) {
+    //     // handle success
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     // handle error
+    //     console.log(error);
+    //   })
+    //   .then(function () {
+    //     // always executed
+    //   });
+    // this.setState({result:this.state.value})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main-app">
+            <p>
+        Try "Hello" or "Nihao" or other whatever you want
+      </p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange}/>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <p>{this.state.result}</p>
       </div>
     );
   }
